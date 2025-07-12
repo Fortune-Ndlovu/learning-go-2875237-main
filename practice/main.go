6828416f6e54152b323fc620acfd291a5484a510
Fortune-Ndlovu <fndlovu@redhat.com>
2025-07-12 11:32:42 +0100

    point
    Signed-off-by: Fortune-Ndlovu <fndlovu@redhat.com>

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	anInt := 42
+	var p = &anInt
+	fmt.Println("Value of p:", *p)
+
+	value1 := 42.13
+	pointer1 := &value1
+	fmt.Println("Value 1:", *&pointer1)
 }
